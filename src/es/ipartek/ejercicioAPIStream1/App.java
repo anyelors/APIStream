7835d3535e1c0044c6a2e2b1a9c6946f03840b6c
anyelors <anyelors@gmail.com>
2025-10-17 23:11:51 +0200

    Clase APIStream #3 Ejercicio

--- src/es/ipartek/ejercicioAPIStream1/App.java
+++ src/es/ipartek/ejercicioAPIStream1/App.java
@@ -1,12 +1,9 @@
 package es.ipartek.ejercicioAPIStream1;
 
-import es.ipartek.Persona;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Comparator;
-import java.util.List;
-import java.util.OptionalInt;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -32,7 +29,7 @@ public class App {
     public static List<String> obtenerCodVuelosByOrigen( List<Vuelo> vuelos, String origen ) {
         return vuelos.stream()
                 .filter(v -> v.getOrigen().equals(origen))
-                .map(v-> v.getCodigoVuelo()).toList();
+                .map(Vuelo::getCodigoVuelo).toList();
     }
 
     public static OptionalInt obtenerVueloMaxPasajeros(List<Vuelo> vuelos) {
@@ -41,7 +38,7 @@ public class App {
                 .max();
     }
 
-    public static List<Vuelo> obtenerVuelosOrdenadoByNumPasajeros( List<Vuelo> vuelos ) {
+    public static List<Vuelo> obtenerVuelosOrderByNumPasajeros(List<Vuelo> vuelos ) {
         return vuelos.stream()
                 .sorted(Comparator.reverseOrder()).toList();
     }
@@ -50,6 +47,60 @@ public class App {
         return vuelos.stream()
                 .mapToDouble(Vuelo::getNumeroPasajeros)
                 .sum();
+    }
+
+    public static List<Vuelo> obtenerVuelosByFechaOrderHoraLLegada( List<Vuelo> vuelos, LocalDate fecha ) {
+        return vuelos.stream()
+                .filter(v -> v.getFechaSalida().equals(fecha))
+                .sorted(Comparator.comparing(Vuelo::getHoraLlegada))
+                .toList();
+    }
+
+    public static String obtenerCodVueloByVueloMaxPasajeros ( List<Vuelo> vuelos ) {
+        return vuelos.stream()
+                .max(Comparator.naturalOrder())
+                .map(Vuelo::getCodigoVuelo)
+                .orElse("No existe");
+    }
+    
+    public static Map<Boolean, List<Vuelo>> obtenerVuelosByNumPasajerosLimit ( List<Vuelo> vuelos, int limitIni, int limitFin ) {
+        return vuelos.stream()
+                .collect(Collectors.partitioningBy(v -> v.getNumeroPasajeros() >= limitIni && v.getNumeroPasajeros() <= limitFin));
+    }
+
+    public static Map<String, List<Vuelo>> obtenerVuelosGroupDestino ( List<Vuelo> vuelos ) {
+        return vuelos.stream()
+                .collect(Collectors.groupingBy(Vuelo::getDestino));
+    }
+
+    public static boolean obtenerVueloMasNumPasajeros ( List<Vuelo> vuelos, int maxPasajeros ) {
+        return vuelos.stream()
+                .anyMatch(v -> v.getNumeroPasajeros() >= maxPasajeros);
+    }
+
+    public static Vuelo obtenerVueloByOrigenMinHoraLlegada( List<Vuelo> vuelos, String destino ) {
+        return vuelos.stream()
+                .filter(v -> v.getDestino().equals(destino))
+                .min(Comparator.comparing(Vuelo::getHoraLlegada))
+                .orElse(null);
+    }
+
+    public static double obtenerMediaPasajerosByDestino( List<Vuelo> vuelos, String destino ) {
+        return vuelos.stream()
+                .filter(v -> v.getDestino().equals(destino))
+                .mapToInt(Vuelo::getNumeroPasajeros)
+                .average()
+                .orElse(0);
+    }
+
+    public static Map<String, Long> obtenerTotalVuelosByDestino ( List<Vuelo> vuelos ) {
+        return vuelos.stream()
+                .collect(Collectors.groupingBy(Vuelo::getDestino, Collectors.counting()));
+    }
+
+    public static Map<String, Long> obtenerTotalNumPasajerosByDestino ( List<Vuelo> vuelos ) {
+        return vuelos.stream()
+                .collect(Collectors.groupingBy(Vuelo::getDestino, Collectors.summingLong(Vuelo::getNumeroPasajeros)));
     }
 
     public static void main(String[] args) {
@@ -78,10 +129,51 @@ public class App {
         System.out.println( maxPasajeros.isPresent() ? "Vuelo con el mayor numero de pasajeros = " + maxPasajeros.getAsInt() : "No existe");
 
         System.out.println("4.- Método que obtiene una lista con todos los vuelos ordenados por número de pasajeros en orden descendente. ");
-        List<Vuelo> vuelosOrdenadosByNumPasajeros = obtenerVuelosOrdenadoByNumPasajeros(vuelos);
+        List<Vuelo> vuelosOrdenadosByNumPasajeros = obtenerVuelosOrderByNumPasajeros(vuelos);
         vuelosOrdenadosByNumPasajeros.forEach(System.out::println);
 
         System.out.println("5.- Método que retorna el total de pasajeros viajando entre todos los vuelos registrados");
         System.out.println(obtenerTotalPasajeros(vuelos));
+
+        System.out.println("6.- Método que retorna todos los vuelos de una determinada fecha indicada ordenados por hora de llegada");
+        obtenerVuelosByFechaOrderHoraLLegada(vuelos, LocalDate.of(2025, 10, 8)).forEach(System.out::println);
+
+        System.out.println("7.- Método que retorne el código del vuelo con más pasajeros registrado");
+        System.out.println(obtenerCodVueloByVueloMaxPasajeros(vuelos));
+
+        System.out.println("8.- Método que retorna un mapa map<Boolean, List<Vuelo>> conteniendo los vuelos con un número de pasajeros por encima y por debajo del indicado. ( emplea Collectors.Partitioning ) )");
+        Map<Boolean, List<Vuelo>> vuelosByNumPasajerosLimit = obtenerVuelosByNumPasajerosLimit(vuelos, 150, 200);
+        System.out.println("Vuelos con numero de pasajeros entre 150 y 200:");
+        vuelosByNumPasajerosLimit.get(true).forEach(System.out::println);
+        System.out.println("Vuelos con numero de pasajeros fuera de 150 y 200:");
+        vuelosByNumPasajerosLimit.get(false).forEach(System.out::println);
+
+        System.out.println("9.- Método que retorna un mapa map<String, List<Vuelo>> conteniendo todos los vuelos agrupados por ciudad de destino");
+        Map<String, List<Vuelo>> vuelosGroupByDestino = obtenerVuelosGroupDestino( vuelos );
+        vuelosGroupByDestino.forEach((c,v) -> {
+            System.out.println("Ciudad Destino: " + c);
+            v.forEach(System.out::println);
+        });
+
+        System.out.println("10.- Método que retorna un valor lógico indicando si hay algún vuelo con más de una determinada cantidad de pasajeros");
+        boolean vMaxPasajeros = obtenerVueloMasNumPasajeros(vuelos, 300);
+        System.out.println( vMaxPasajeros ? "Existen vuelos con más de 300 Pasajeros" : "No Existen vuelos con mas de 500 Pasajeros");
+
+        System.out.println("11.- Método que retorna el primer vuelo que llega a una determinada ciudad de destino");
+        Vuelo vueloByOrigenMinHoraLlegada = obtenerVueloByOrigenMinHoraLlegada( vuelos, "Londres");
+        System.out.println(vueloByOrigenMinHoraLlegada);
+
+        System.out.println("12.- Método que retorna la media de pasajeros que viajan a una determinada ciudad de destino.");
+        double mediaPasajerosByDestino = obtenerMediaPasajerosByDestino( vuelos, "Roma");
+        System.out.println("Media de pasajeros a Roma: " + mediaPasajerosByDestino );
+
+        System.out.println("13.- Método que retorna un mapa map<String, Long> que retorna el total de vuelos por cada ciudad de destino.");
+        Map<String, Long> totalVuelosByDestino = obtenerTotalVuelosByDestino( vuelos );
+        totalVuelosByDestino.forEach((c,v) -> System.out.println("Ciudad Destino: " + c + " - Total Vuelos: " + v));
+
+        System.out.println("14.- Método que retorna un mapa map<String, Long> que retorna el total de pasajeros que viajan en los vuelos a cada ciudad de destino");
+        Map<String, Long> totalNumPasajerosByDestino = obtenerTotalNumPasajerosByDestino( vuelos );
+        totalNumPasajerosByDestino.forEach((c,p) -> System.out.println("Ciudad Destino: " + c + " - Total Pasajeros: " + p));
+
     }
 }
